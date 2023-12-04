41fe719f9f0b86a1fa4b5ff77009ea66ca758875
Juan Camilo <jpcamilo407@gmail.com>
2023-12-03 21:55:35 -0500

    BackEnd initializr proyect

---
+++ inventariosBackEnd/src/main/java/gm/inventariosBackEnd/InventariosBackEndApplication.java
@@ -0,0 +1,13 @@
+package gm.inventariosBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventariosBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventariosBackEndApplication.class, args);
+	}
+
+}
---
+++ inventariosBackEnd/src/test/java/gm/inventariosBackEnd/InventariosBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package gm.inventariosBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventariosBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
